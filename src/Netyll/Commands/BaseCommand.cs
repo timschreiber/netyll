8e97d1fa05b28cf95938b425d7f75b7d2c556581
Tim Schreiber <timschreiber@users.noreply.github.com>
2021-04-29 22:57:30 -0400

    Added NewCommand functionality and barebones blank scaffold

--- src/Netyll/Commands/BaseCommand.cs
+++ src/Netyll/Commands/BaseCommand.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.IO;
 
 namespace Netyll.Commands
 {
     public abstract class BaseCommand
     {
+        protected DirectoryInfo CurrentDirectory => new DirectoryInfo(Environment.CurrentDirectory);
+
         public abstract int Run();
     }
 }
--- src/Netyll/Commands/NewCommand.cs
+++ src/Netyll/Commands/NewCommand.cs
@@ -1,15 +1,40 @@
-﻿using System;
+﻿using Microsoft.Extensions.FileProviders;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using System.Text;
 
 namespace Netyll.Commands
 {
     public class NewCommand : BaseCommand
     {
+        private readonly EmbeddedFileProvider _embeddedFileProvider;
+
+        public NewCommand()
+        {
+            _embeddedFileProvider = new EmbeddedFileProvider(Assembly.GetExecutingAssembly());
+        }
+
         public override int Run()
         {
+            copyResource("Scaffolds/blank/_config.yml", $"{CurrentDirectory.FullName}\\_config.yml");
+            copyResource("Scaffolds/blank/index.html", $"{CurrentDirectory.FullName}\\index.html");
+            var layoutsDirectory = CurrentDirectory.CreateSubdirectory("_layouts");
+            copyResource("Scaffolds/blank/_layouts/default.html", $"{layoutsDirectory.FullName}\\default.html");
             Console.WriteLine($"{nameof(NewCommand)}.{nameof(Run)}");
             return 0;
+        }
+
+        private void copyResource(string resourceName, string fileName)
+        {
+            using (var resourceStream = _embeddedFileProvider.GetFileInfo(resourceName).CreateReadStream())
+            using (var fileStream = File.Create(fileName))
+            {
+                resourceStream.CopyTo(fileStream);
+                fileStream.Close();
+                resourceStream.Close();
+            }
         }
     }
 }
